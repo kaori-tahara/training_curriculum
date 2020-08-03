f9481c955c1fde74a3e2877cac3e1ac98173dbbf
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-03 11:31:28 +0900

    issue5,6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,7 +4,6 @@ class CalendarsController < ApplicationController
   def index
    get_week
    @plan = Plan.new
- 
 
   end
 
@@ -21,10 +20,13 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date,:plan)
   end
 
+
+  
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+    
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
@@ -37,9 +39,22 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, day:(wdays[(@todays_date+x).wday]), plans: plans}
       @week_days.push(days)
     end
 
   end
 end
+
+# wdays[(@todays_date + x).wday]
+# # wdays[1]
+# wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+# wdays[(@todays_date + x).wday]
+# wdays[1]
+# wdays[2]　　xは7timesのブロック変数で曜日の回数を表している
+
+# (@todays_date + x).wday
+# (1 + 1) * 3 = 
+
+# プライベートにget_weekを入れているのは、見やすくするため。プライベートの下にメゾットをおいても、ok
+# メゾット名を呼び出して使える。
