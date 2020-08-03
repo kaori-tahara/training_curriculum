f1de7b77d3e00cf6c8505f7c53f0a1107a1c9096
kaori-tahara <68538917+kaori-tahara@users.noreply.github.com>
2020-08-03 12:36:07 +0900

    first_training revert

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -17,13 +17,10 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-
-  
-  def get_week
+  def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    
+
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-    
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
@@ -42,16 +39,3 @@ class CalendarsController < ApplicationController
 
   end
 end
-
-# wdays[(@todays_date + x).wday]
-# # wdays[1]
-# wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-# wdays[(@todays_date + x).wday]
-# wdays[1]
-# wdays[2]　　xは7timesのブロック変数で曜日の回数を表している
-
-# (@todays_date + x).wday
-# (1 + 1) * 3 = 
-
-# プライベートにget_weekを入れているのは、見やすくするため。プライベートの下にメゾットをおいても、ok
-# メゾット名を呼び出して使える。
